499f459092f9e236e612b8a0f8b50bc32e8199d6
Surajit Sarkar <suro@Surajits-MacBook-Pro-2.local>
2024-01-02 16:55:31 +0530

    Basic sample mobile gestures

--- AppiumCSLearning/IOSTests.cs
+++ AppiumCSLearning/IOSTests.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Interfaces;
 using OpenQA.Selenium.Appium.iOS;
 
 namespace AppiumCSLearning
 {
 	public class IOSTests
 	{
-        AppiumDriver<IOSElement> driver;
+        AppiumDriver<IWebElement> driver;
 
         [SetUp]
         public void Setup()
@@ -20,7 +22,7 @@ namespace AppiumCSLearning
             options.AddAdditionalCapability("appium:deviceName", "iPhone 15 Pro Max");
             options.AddAdditionalCapability("appium:app", appPath);
             options.AddAdditionalCapability("appium:newCommandTimeout", "120000");
-            driver = new IOSDriver<IOSElement>(new Uri("http://127.0.0.1:4723"), options);
+            driver = new IOSDriver<IWebElement>(new Uri("http://127.0.0.1:4723"), options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
         }
 
@@ -28,11 +30,25 @@ namespace AppiumCSLearning
         public void IOSTest1()
         {
             TestContext.Progress.WriteLine("App Launched...");
+            IWebElement item = driver.FindElementByName("Sauce Labs Backpack");
+            Thread.Sleep(2000);
+            MobileGestures.PerformTapByTA(driver,item);
+            Thread.Sleep(5000);
+        }
+        [Test]
+        public void Test2()
+        {
+            TestContext.Progress.WriteLine("App Launched...");
+            driver.TerminateApp("com.saucelabs.mydemoapp.rn");
+            Thread.Sleep(5000);
+            MobileGestures.PerformSwipeDownByTA(driver);
+            TestContext.Progress.WriteLine("Swiped Down");
+            Thread.Sleep(5000);
         }
         [TearDown]
         public void Teardown()
         {
-            driver.TerminateApp("com.saucelabs.mydemoapp.rn");
+            //driver.TerminateApp("com.saucelabs.mydemoapp.rn");
             driver.Quit();
         }
     }
